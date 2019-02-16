fd962614d6624188d196551d3ccd68ef9b3f0ae7
Efynox <efynox@live.fr>
2019-02-16 12:54:15 +0100

    remove index.html minification

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  filenameHashing: false
+  filenameHashing: false,
+  chainWebpack: config => {
+    config.plugin("html").tap(args => {
+      args[0].minify = false;
+      return args;
+    });
+  }
 };
