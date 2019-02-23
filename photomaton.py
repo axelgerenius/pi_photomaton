c2b5b1da3e4ae8426c20eb3c6ccd8dc3f9bb8fc9
Matthieu Holtz <matthieu.holtz@gmail.com>
2019-02-23 08:56:49 +0000

    + Remove the GPIO support for the LED

--- photomaton.py
+++ photomaton.py
@@ -3,7 +3,6 @@
 import sys
 import os
 import time
-import RPi.GPIO as GPIO
 import paho.mqtt.client as mqtt
 import json
 from enum import Enum
@@ -11,88 +10,15 @@ from subprocess import call
 from subprocess import Popen
 from stat import *
 
-RED_LED_GPIO = 17
-BLUE_LED_GPIO = 22
-GREEN_LED_GPIO = 27
-
-BUTTON_GPIO = 4
 
 FILE_SAVE_PATH = "/var/photomaton-www/images/"
 FILE_SAVE_PATH_THUMBNAILS = FILE_SAVE_PATH + "thumbnails/"
 
 client = mqtt.Client("pythonClient")
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-
-class Color(Enum):
-	Red = 0
-	Green = 1
-	Blue = 2
-	Yellow = 3
-	Magenta = 4
-	Cyan = 5
-	White = 6
-	Off = 7
-
-def applyColor(_color):
-
-	if _color == Color.Red:
-		GPIO.output(RED_LED_GPIO, GPIO.HIGH)
-		GPIO.output(BLUE_LED_GPIO, GPIO.LOW)
-		GPIO.output(GREEN_LED_GPIO, GPIO.LOW)
-
-	elif _color == Color.Green:
-		GPIO.output(RED_LED_GPIO, GPIO.LOW)
-		GPIO.output(BLUE_LED_GPIO, GPIO.LOW)
-		GPIO.output(GREEN_LED_GPIO, GPIO.HIGH)
-
-	elif _color == Color.Blue:
-		GPIO.output(RED_LED_GPIO, GPIO.LOW)
-		GPIO.output(BLUE_LED_GPIO, GPIO.HIGH)
-		GPIO.output(GREEN_LED_GPIO, GPIO.LOW)
-
-	elif _color == Color.Yellow:
-		GPIO.output(RED_LED_GPIO, GPIO.LOW)
-		GPIO.output(BLUE_LED_GPIO, GPIO.HIGH)
-		GPIO.output(GREEN_LED_GPIO, GPIO.HIGH)
-
-	elif _color == Color.Magenta:
-		GPIO.output(RED_LED_GPIO, GPIO.HIGH)
-		GPIO.output(BLUE_LED_GPIO, GPIO.HIGH)
-		GPIO.output(GREEN_LED_GPIO, GPIO.LOW)
-
-	elif _color == Color.Cyan:
-		GPIO.output(RED_LED_GPIO, GPIO.LOW)
-		GPIO.output(BLUE_LED_GPIO, GPIO.HIGH)
-		GPIO.output(GREEN_LED_GPIO, GPIO.HIGH)
-
-	elif _color == Color.White:
-		GPIO.output(RED_LED_GPIO, GPIO.HIGH)
-		GPIO.output(BLUE_LED_GPIO, GPIO.HIGH)
-		GPIO.output(GREEN_LED_GPIO, GPIO.HIGH)
-
-	elif _color == Color.Off:
-		GPIO.output(RED_LED_GPIO, GPIO.LOW)
-		GPIO.output(BLUE_LED_GPIO, GPIO.LOW)
-		GPIO.output(GREEN_LED_GPIO, GPIO.LOW)
-
-def buttonPressed():
-	return GPIO.input(BUTTON_GPIO) == GPIO.HIGH
-
-def initGPIO():
-	GPIO.setup(RED_LED_GPIO, GPIO.OUT)
-	GPIO.setup(BLUE_LED_GPIO, GPIO.OUT)
-	GPIO.setup(GREEN_LED_GPIO, GPIO.OUT)
-
-	GPIO.setup(BUTTON_GPIO, GPIO.IN)
 
 def capture():
-	applyColor(Color.Cyan)
 	filename = time.strftime('%d-%m-%y_%H-%M-%S')
-
-	#call(["raspistill", "-o", filename ])
-	#call(["cp", filename, FILE_SAVE_PATH + "last.jpg"])
 
 #	ret=call(["raspistill", "-o", FILE_SAVE_PATH+filename+".jpg" ])
 
@@ -107,9 +33,6 @@ def capture():
 	p = Popen(["convert", "-thumbnail", "200x200", FILE_SAVE_PATH+filename+".jpg", FILE_SAVE_PATH_THUMBNAILS+filename+".jpg"])
 
 	client.publish("photomaton/newPhoto",filename + '.jpg')
-	applyColor(Color.Red)
-	time.sleep(3)
-	applyColor(Color.Green)
 
 
 def photo_list():
@@ -127,6 +50,7 @@ def photo_list():
 	jsonfied = json.dumps(computed, ensure_ascii=False)
 	client.publish("photomaton/list_result",jsonfied)
 
+
 def on_message(client, userdata, message):
 	print("message received " ,str(message.payload.decode("utf-8")))
 	print("message topic=",message.topic)
@@ -141,10 +65,8 @@ def on_message(client, userdata, message):
 		print("Topic list")
 		photo_list()
 
-def main():
-	initGPIO()
-	applyColor(Color.Green)
 
+def main():
 	client.connect("127.0.0.1")
 	client.on_message=on_message #attach function to callback
 	client.loop_start() #start the loop
@@ -152,10 +74,6 @@ def main():
 	client.subscribe("photomaton/list")
 	client.publish("photomaton/started", "started")
 	while True:
-		if buttonPressed() == True:
-			client.publish("photomaton/button", "pressed")
-#			capture()
-
 		time.sleep(10/1000)
 
 	client.loop_stop() #stop the loop
